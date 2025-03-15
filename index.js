49babc0ff90b372ba9425e1bc9624999fb95d093
Farid iskandar <fariditb159@gmail.com>
2025-03-15 19:31:09 +0700

    update struktur folder

--- index.js
+++
@@ -1,8 +0,0 @@
-import app from "./src/app/api.js";
-import Logger from "./src/app/logger.js";
-
-const port = 5000;
-
-app.listen(port, () => {
-    Logger.info(`server is running on http://localhost:${port}`);
-})
